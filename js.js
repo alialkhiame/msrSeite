40774d3386f2bd20e06870b1bcce2cde5d8bcefb
alialkhiame <32980384+alialkhiame@users.noreply.github.com>
2023-08-27 17:48:37 +0200

    bars are okay

---
+++ js.js
@@ -0,0 +1,95 @@
+ 
+$('#first-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+	  nav: false, // Hide navigation buttons
+  dots: false, // Hide dots
+    autoplay: true, // Enable autoplay
+  autoplayTimeout: 3000, // Set the timeout to 3 seconds
+  autoplayHoverPause: true, // Pause on hover
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#second-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+	  nav: false, // Hide navigation buttons
+  dots: false, // Hide dots
+    autoplay: true, // Enable autoplay
+  autoplayTimeout: 3000, // Set the timeout to 3 seconds
+  autoplayHoverPause: true, // Pause on hover
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+  
+$('#third-carousel').owlCarousel({
+    rtl:false,
+    loop:true,
+    margin:10,
+	  nav: false, // Hide navigation buttons
+  dots: false, // Hide dots
+    autoplay: true, // Enable autoplay
+  autoplayTimeout: 3000, // Set the timeout to 3 seconds
+  autoplayHoverPause: true, // Pause on hover
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#third-carousel').owlCarousel({
+    rtl:false,
+    loop:true,
+    margin:10,
+	  nav: false, // Hide navigation buttons
+  dots: false, // Hide dots
+    autoplay: true, // Enable autoplay
+  autoplayTimeout: 3000, // Set the timeout to 3 seconds
+  autoplayHoverPause: true, // Pause on hover
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+ 
